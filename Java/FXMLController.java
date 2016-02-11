f8b8ccc4cb5c73e85cb7437caab40e5172a0ab2c
Abubakr Iqbal <abumiqb@gmail.com>
2016-02-11 16:40:01 +0100

    Lagt grid, colorPicker/tegne på canvas.
    Mangler: Selvstendig farge på grid og pen, gjøre grid firkantet og behagelig for øyne.
    
    //Abu.

--- Java/FXMLController.java
+++ Java/FXMLController.java
@@ -9,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.MenuBar;
 import javafx.scene.paint.Color;
 import javafx.scene.input.MouseEvent;
 
@@ -16,22 +17,30 @@ import javafx.scene.input.MouseEvent;
 public class FXMLController implements Initializable 
 {
     @FXML private Canvas canvas;
-    GraphicsContext gc;
-    
+    @FXML private ColorPicker colorPicker;
+    @FXML private MenuBar menubar;
+    GraphicsContext graphicsContext;
+    double canvasWidth;
+    double canvasHeight;
+    int[][] array = new int[45][50];
     @Override
     public void initialize(URL location, ResourceBundle resources) 
     {
       
-        gc = canvas.getGraphicsContext2D();
-        gc.setFill(Color.WHITE);
-        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        gc.strokeRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        graphicsContext = canvas.getGraphicsContext2D();
+        canvasWidth = canvas.getWidth();
+        canvasHeight = canvas.getHeight();
+        graphicsContext.setLineWidth(3);
+        graphicsContext.setFill(Color.WHITE);
+        graphicsContext.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        graphicsContext.strokeRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        colorPicker.setValue(Color.RED);
+       
+       graphicsContext.setStroke(colorPicker.getValue());
       
+        Tegne();
         
-        final GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        Tegne(graphicsContext);
-         
-        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, (MouseEvent event) -> 
+         canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, (MouseEvent event) -> 
         {
             graphicsContext.beginPath();
             graphicsContext.moveTo(event.getX(), event.getY());
@@ -40,46 +49,60 @@ public class FXMLController implements Initializable
          
         canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, (MouseEvent event) ->
         {
+             
             graphicsContext.lineTo(event.getX(), event.getY());
             graphicsContext.stroke();
         });
  
         canvas.addEventHandler(MouseEvent.MOUSE_RELEASED, (MouseEvent event) -> 
         {
+            //kodes seinere
             //Ingenting skjer når musen slippes
         }
     );
         
     }
     
-    private void Tegne(GraphicsContext gc)
+    private void Tegne()
     {
-        double canvasWidth = gc.getCanvas().getWidth();
-        double canvasHeight = gc.getCanvas().getHeight();
+      array[1][1] = 1;
+      array[0][2] = 1;
+        //graphicsContext.setFill(Color.WHITE);
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array.length; j++){
+                if(array[i][j] == 1) graphicsContext.fillRect(i*canvasWidth/array.length, j*canvasHeight/array.length, canvasWidth/array.length, canvasHeight/array.length);
+                graphicsContext.strokeRect(i*canvasWidth/array.length, j*canvasHeight/array.length, canvasWidth/array.length, canvasHeight/array.length);
+                
+            }
+        }
        
-        gc.fill();
-        gc.strokeRect
-        (
-            0,              //x øverst venstre hjørne
-            0,              //y øverst venstre hjørne
-            canvasWidth,    //bredde canvas
-            canvasHeight);  //høyde canvas
-        
-       final ColorPicker colorPicker = new ColorPicker();
-       
-       colorPicker.setValue(Color.RED);
-       
-       gc.setStroke(colorPicker.getValue());
-       
-        colorPicker.setOnAction((ActionEvent t) -> {
-            gc.setStroke(colorPicker.getValue());
-        });
-       
-       // gc.setStroke(Color.BLUE);
-        gc.setLineWidth(5);     
-         
+        colorPicker.setOnAction((ActionEvent t) -> 
+        {
+            graphicsContext.setStroke(colorPicker.getValue());
+        }
+
+     );
+              
     }
+    
+     public void nullstillGameboard (ActionEvent event)
+   {
+      
+       //graphicsContext.setFill(Color.WHITE);
+       graphicsContext.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+       //graphicsContext.setStroke(Color.BLACK);
+       graphicsContext.strokeRect(0, 0, canvas.getWidth(), canvas.getHeight()); 
+       Tegne();
+        
+
+   }
+    
+  
 }
+
+
+
+
 
 
 
